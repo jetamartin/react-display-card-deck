b89625be240c4cd1fb41d565913bcf4469bb32d2
Jet Martin <jetamartin@gmail.com>
2021-04-22 21:14:53 -0700

    Added styling and changed 'Hit Me' button to deal cards automatically 1/second

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import './Card.css'
 const Card = ({name, image}) => {
+
+  const [{angle, xPos, yPos}] = useState({
+    angle: Math.random() * 90 - 45,
+    xPos: Math.random() * 40 - 20,
+    yPos: Math.random() * 40 - 20
+  });
   // console.log(card)
   return (
-    <img
+    <img className="Card"
        alt={name}
        src={image}
+       style={{transform: `rotate(${angle}deg) translate(${xPos}px, ${yPos}px)` }}
     />
   )
 }
--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import Card from './Card';
+import './CardDeck.css'
 
 const CardDeck = () => {
   const [ deck, setDeck ] = useState(null); // Deck available from API
   const [ card, setCard ] = useState([]);   // Holds cards from API
-  const [ draw, setDraw ] = useState(false); //
-  const [ cardsRemaining, setCardsRemaining ] = useState(true);
-  const [ cardCount, setCardCount] = useState(0);
- 
-
+  const [ draw, setDraw ] = useState(false); // Boolean for card draw button 
+  const [ cardsRemaining, setCardsRemaining ] = useState(true); // Boolean to track if any cards left to draw.
+  const [ cardCount, setCardCount] = useState(0); // Keep track of number of cards drawn
+  const [ autoDraw, setAutoDraw ] = useState(false)
+  const timerRef = useRef(null);
   const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
  
   // Get new deck of cards once after component rendered
@@ -26,16 +27,19 @@ const CardDeck = () => {
       if (deck) { // After initial render card deck won't be available from API call
         let { deck_id } = deck;
         try {
-        // Retrieve a card from the deck 
-        let cardDrawnData = await axios.get((`${API_BASE_URL}/${deck_id}/draw/`)); 
+
         // Check to see if any cards remain to be displayed if not then skip display
         if (cardsRemaining) {  
+
+          // Retrieve a card from the deck 
+          let cardDrawnData = await axios.get((`${API_BASE_URL}/${deck_id}/draw/`)); 
 
           // Card #52 will have cardsRemaining = 0, so set cardsRemaing flag to false
           if (cardDrawnData.data.remaining === 0) {
             // When no cards remaining we will disable button in the view to request more cards
             setCardsRemaining(false);
-            throw new Error("Error: No cards remaining!")
+            setAutoDraw(false);
+            // throw new Error("Error: No cards remaining!")
           }
           // Used to display number of cards dealt to user 
           setCardCount(cardCount => cardCount + 1);
@@ -54,18 +58,34 @@ const CardDeck = () => {
             }
           ]);
           }
+
         } catch (err) {
           alert(err)
         }
       } 
 
     }
-    getNewCard();
-  }, [draw]);
+
+    if (autoDraw && !timerRef.current) {
+      timerRef.current = setInterval(async () => {
+        await getNewCard();
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timerRef.current); 
+      timerRef.current = null;
+    }
+
+  }, [draw, autoDraw]);
   
+    // Function associated with Part 1 of exercise:
     const dealCard = () => {
       // Toggle draw state each time button is clicked to cause rerender of component to display new card
        setDraw(!draw)
+    }
+
+    const autoDealCard = () => {
+      setAutoDraw(!autoDraw)
     }
 
     // Build array of card components for each card 
@@ -74,20 +94,19 @@ const CardDeck = () => {
     )); 
 
   return (
-    <div>
+    <div className="CardDeck">
 
-      {!cardsRemaining ? <span>Sorry, No more cards</span> :
+      {/* {!cardsRemaining ? <span>Sorry, No more cards</span> : */}
       <div>
-        <button onClick={dealCard} disabled={!cardsRemaining} >
-        {/* <button onClick={dealCard}  > */}
-
+        { !cardsRemaining ? <div>No more cards in this Deck - Refresh Browswer for a new deck</div> : ""}
+        <button onClick={autoDealCard} disabled={!cardsRemaining} >
             Hit Me!
         </button>
-        <br></br>
+        <br/>
         <span>{cardCount}</span>
-        <div>{cards}</div>
+        <div className="CardDeck-cards">{cards}</div>
       </div>
-      } 
+      {/* }  */}
 
     </div>
   )
